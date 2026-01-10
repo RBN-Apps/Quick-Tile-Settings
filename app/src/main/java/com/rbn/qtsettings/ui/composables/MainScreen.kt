c7dc31f386c7bc28c17929d8f0ce657c8f14b7ec
RBN-Apps <80348653+RBN-Apps@users.noreply.github.com>
2026-01-10 12:15:09 +0100

    Remove deprecated TabRow

--- app/src/main/java/com/rbn/qtsettings/ui/composables/MainScreen.kt
+++ app/src/main/java/com/rbn/qtsettings/ui/composables/MainScreen.kt
@@ -25,10 +25,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SecondaryTabRow
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -131,7 +131,7 @@ fun MainScreen(
                 })
             }
 
-            TabRow(selectedTabIndex = pagerState.currentPage) {
+            SecondaryTabRow(selectedTabIndex = pagerState.currentPage) {
                 tabTitles.forEachIndexed { index, title ->
                     Tab(
                         selected = pagerState.currentPage == index,
