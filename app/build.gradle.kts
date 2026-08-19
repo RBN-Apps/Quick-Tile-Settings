042ecbd9c49b8eab0d55c59ddeec66ad4f15cbd0
RBN-Apps <80348653+RBN-Apps@users.noreply.github.com>
2026-08-19 20:04:35 +0200

    prepare 1.4.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.rbn.qtsettings"
         minSdk = 29
         targetSdk = 37
-        versionCode = 8
-        versionName = "1.3.0"
+        versionCode = 9
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments += mapOf(
